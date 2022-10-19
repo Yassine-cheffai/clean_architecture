907a4ba790607ac2aadc5f85a2fd7d683469f8c2
Yassine-cheffai <yacincheffai@yahoo.com>
2022-10-19 09:10:43 +0200

    unit tests for samplecalculator

--- calculator/main.py
+++ calculator/main.py
@@ -1,6 +1,15 @@
+from functools import reduce
+
+
 class SimpleCalculator:
-    def add(self, *args):
+    def add(self, *args) -> int:
         """
         calculate the sum of multiple arguments
         """
         return sum(args)
+
+    def sub(self, a: int, b: int) -> int:
+        return a - b
+
+    def mul(self, *args) -> int:
+        return reduce(lambda x, y: x * y, args)
--- calculator/test_main.py
+++ calculator/test_main.py
@@ -6,13 +6,34 @@ def test_add_two_numbers():
     result = calculator.add(4, 5)
     assert result == 9
 
+
 def test_add_three_numbers():
     calculator = SimpleCalculator()
     result = calculator.add(4, 5, 6)
     assert result == 15
+
 
 def test_add_many_numbers():
     numbers = range(100)
     calculator = SimpleCalculator()
     result = calculator.add(*numbers)
     assert result == 4950
+
+
+def test_sub_two_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.sub(10, 7)
+    assert result == 3
+
+
+def test_mul_two_numbers():
+    calculator = SimpleCalculator()
+    result = calculator.mul(6, 4)
+    assert result == 24
+
+
+def test_mul_many_numbers():
+    calculator = SimpleCalculator()
+    numbers = range(1, 10)
+    result = calculator.mul(*numbers)
+    assert result == 362880
