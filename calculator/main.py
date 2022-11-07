ae61a4afb1c87ab5fcd2da30e65a8648ce680629
Yassine-cheffai <yacincheffai@yahoo.com>
2022-11-07 19:55:55 +0100

    update

--- calculator/main.py
+++ calculator/main.py
@@ -12,4 +12,35 @@ class SimpleCalculator:
         return a - b
 
     def mul(self, *args) -> int:
+        if 0 in args:
+            raise ValueError
         return reduce(lambda x, y: x * y, args)
+
+    def div(self, a: int, b: int) -> float:
+        if not b:
+            return float("inf")
+        return a / b
+
+    def avg(
+        self, values: list[int], lt: None | int = None, ut: None | int = None
+    ) -> float:
+        filtred_values = []
+        if ut and lt:
+            for value in values:
+                if value in range(lt, ut + 1):
+                    filtred_values.append(value)
+            return sum(filtred_values) / len(filtred_values) if filtred_values else 0
+
+        if ut and not lt:
+            for value in values:
+                if value <= ut:
+                    filtred_values.append(value)
+            return sum(filtred_values) / len(filtred_values) if filtred_values else 0
+
+        if not ut and lt:
+            for value in values:
+                if value >= lt:
+                    filtred_values.append(value)
+            return sum(filtred_values) / len(filtred_values) if filtred_values else 0
+
+        return sum(values) / len(values) if values else 0
--- calculator/test_main.py
+++ calculator/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from .main import SimpleCalculator
 
 
@@ -37,3 +39,46 @@ def test_mul_many_numbers():
     numbers = range(1, 10)
     result = calculator.mul(*numbers)
     assert result == 362880
+
+def test_div_two_numbers_float():
+    calculator = SimpleCalculator()
+    result = calculator.div(13, 2)
+    assert result == 6.5
+
+def test_div_by_zero_return_inf():
+    calculator = SimpleCalculator()
+    result = calculator.div(5, 0)
+    assert result == float("inf")
+
+def test_mul_by_zero_raises_exception():
+    calculator = SimpleCalculator()
+    with pytest.raises(ValueError):
+        calculator.mul(3, 0)
+
+def test_avg_simple():
+    calculator = SimpleCalculator()
+    assert calculator.avg([2, 5, 12, 98]) == 29.25
+
+def test_avg_with_upper_threshold():
+    calculator = SimpleCalculator()
+    assert calculator.avg([2, 5, 12, 98], ut=90) == calculator.avg([2, 5, 12])
+
+def test_avg_with_lower_threshold():
+    calculator = SimpleCalculator()
+    assert calculator.avg([2, 5, 12, 98], lt=10) == calculator.avg([12, 98])
+
+def test_avg_with_lower_and_upper_threshold():
+    calculator = SimpleCalculator()
+    assert calculator.avg([2, 5, 12, 98], lt=10, ut=90) == calculator.avg([12])
+
+def test_avg_with_empty_list():
+    calculator = SimpleCalculator()
+    assert calculator.avg([]) == 0
+
+def test_avg_with_empty_list_after_limiting():
+    calculator = SimpleCalculator()
+    assert calculator.avg([12, 98], lt=15, ut=90) == 0
+
+def test_avg_with_empty_list_and_ut_lt():
+    calculator = SimpleCalculator()
+    assert calculator.avg([], lt=15, ut=90) == 0
